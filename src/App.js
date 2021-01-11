23e6b6faf58dc54031416684446415974b2052fb
s7sjuhas <sjuhas@s7designcreative.com>
2021-01-11 14:24:29 +0100

    showe hide elements jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. !! !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+
+    const name = 'Larry';
+    const showHello = true;
+
+    const showMath = true;
+
+    const num1 = 40;
+    const num2 = 23;
+
+    let math;
+
+    if(showMath){
+      math = <h4> {num1}+{num2} = { num1 + num2 }</h4>;
+    } else {
+      math = null;
+    }
+
+
+    return (
+
+
+        <div className="App">
+        {showHello ?  <h1>Hello { name.toUpperCase()} </h1> :  <h1>Hello  user</h1>}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. !! !!!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React 
+          </a>
+
+          {math}
+         
+        </header>
+      </div>
+    )
+
+  };
 }
 
 export default App;
