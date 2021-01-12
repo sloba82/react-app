c796f4aefb67f5f7992543a2d4414b42af8adfc2
s7sjuhas <sjuhas@s7designcreative.com>
2021-01-12 10:38:04 +0100

    working with props adding style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 //functional component
 const Header = (props) => {
-    
+    const { branding } = props;
     return (
         <div>
-            <h1>{props.branding}</h1>
+            <h1 style= {headingStyle}>{branding}</h1>
         </div>
     );
 };
 
+Header.defaultProps = {
+    branding: 'My App'
+}
 
+Header.propTypes = {
+    branding: PropTypes.string.isRequired
+}
+
+
+const headingStyle = {
+    color: 'red', 
+    fontSize: '50px'
+};
 export default Header;
